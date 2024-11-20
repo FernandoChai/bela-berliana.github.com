bec7549b389249b054fe5b89ea4c9a126a0d9832
Fernando Chai <fernando.chai@dana.id>
2024-11-21 00:24:07 +0700

    add loading page animation

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function toggleMusic() {
     }
     isPlaying = !isPlaying;
 }
+
+// Update the progress bar during the page loading process
+document.onreadystatechange = function () {
+    const progressBar = document.getElementById('progressBar');
+  
+    if (document.readyState === 'interactive') {
+      // Page DOM is loaded but not all resources (like images) are loaded
+      progressBar.style.width = '50%';
+      progressBar.setAttribute('aria-valuenow', 50);
+    } else if (document.readyState === 'complete') {
+      // Page is fully loaded
+      progressBar.style.width = '100%';
+      progressBar.setAttribute('aria-valuenow', 100);
+  
+      // Hide the overlay after a short delay for a smooth transition
+      setTimeout(() => {
+        document.getElementById('loadingOverlay').classList.add('hidden');
+      }, 500); // Adjust delay if needed
+    }
+  };
+  
+  
